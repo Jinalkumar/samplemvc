172c7d07d2e8e1867dadde5b4ef906bbacccee84
Jinal <Jinal@JSN>
2016-08-16 15:26:36 -0400

    Add project files.

---
+++ final-practise/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(final_practise.Startup))]
+namespace final_practise
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
